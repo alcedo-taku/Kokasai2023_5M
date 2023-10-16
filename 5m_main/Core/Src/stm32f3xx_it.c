5ece1c16af97e55a578f2b88c6f11136b80fc898
takuji.coroton.thanks <taku.tatewaki@gmail.com>
2023-10-16 22:17:21 +0900

    add: (main)主にgpioのピン配置を追加した

--- 5m_main/Core/Src/stm32f3xx_it.c
+++ 5m_main/Core/Src/stm32f3xx_it.c
@@ -57,7 +57,6 @@
 /* External variables --------------------------------------------------------*/
 extern CAN_HandleTypeDef hcan;
 extern TIM_HandleTypeDef htim17;
-extern DMA_HandleTypeDef hdma_usart2_rx;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -199,20 +198,6 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f3xx.s).                    */
 /******************************************************************************/
-
-/**
-  * @brief This function handles DMA1 channel6 global interrupt.
-  */
-void DMA1_Channel6_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Channel6_IRQn 0 */
-
-  /* USER CODE END DMA1_Channel6_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart2_rx);
-  /* USER CODE BEGIN DMA1_Channel6_IRQn 1 */
-
-  /* USER CODE END DMA1_Channel6_IRQn 1 */
-}
 
 /**
   * @brief This function handles USB high priority or CAN_TX interrupts.
