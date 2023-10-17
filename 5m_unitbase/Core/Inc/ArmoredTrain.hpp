9c98f97acd57b97a8b00bf681f64a1e2070ee2db
takuji.coroton.thanks <taku.tatewaki@gmail.com>
2023-10-17 21:33:07 +0900

    add: (unit)装甲列車クラスに関数を2つ追加した

--- 5m_unitbase/Core/Inc/ArmoredTrain.hpp
+++ 5m_unitbase/Core/Inc/ArmoredTrain.hpp
@@ -34,35 +34,57 @@ struct FieldData{
 	static constexpr float gravity = 9.8;			//<! 重力加速度
 }field_data;
 
+struct TargetPositionA {
+	float x,y,angle;
+};
+
 struct RobotStaticData {
-	float angle_of_depression;			//<! 砲塔俯角[rad]
-	float turret_length;				//<! 旋回中心から速度計側部までの距離[m]
+	static constexpr float angle_of_depression = 12;	//<! 砲塔俯角[rad]
+	static constexpr float turret_length = 0.1;			//<! 旋回中心から速度計側部までの距離[m] (l0)
+	static constexpr float radius_of_roller = 0.05;		//<! ローラー半径[m]
+	static constexpr std::array<TargetPositionA, 3> target = {{	//<! ロボット内座標
+			{0.01, 0.01, 12},
+			{0.01, 0.01, 12},
+			{0.01, 0.01, 12},
+	}};
+}robot_static_data;
+
+struct TargetPositionR {
+	float l,angle;
 };
 
 struct RobotMovementData {
-	float position;						//<! 位置[m]
-	float velocity;						//<! 速度[m/s]
-	float angle_of_turret;				//<! 砲塔角度[rad]
-	float angular_velocity_of_truret;	//<! 砲塔角速度[rad/s]
-	float angle_of_depression;			//<! 砲塔俯角[rad]
-	float roller_rotation;				//<! ローラー回転数[rad/s]
+	float position;							//<! 位置[m]
+	float velocity;							//<! 速度[m/s]
+	float angle_of_turret;					//<! 砲塔角度[rad]
+	float angular_velocity_of_truret;		//<! 砲塔角速度[rad/s]
+	float angle_of_depression;				//<! 砲塔俯角[rad]
+	float roller_rotation;					//<! ローラー回転数[rad/s]
+};
+
+struct BulletVelocity {
+	float x,y,z;
 };
 
 constexpr uint16_t frequency = 1000;
+
 
 class ArmoredTrain {
 private:
 	RobotMovementData myself;		//<! 自分の動き
 	RobotMovementData enemy;		//<! 敵の動き
+	std::array<TargetPositionR, 3> target;	//<! 的の位置
+	BulletVelocity bullet_velocity;
 	InputData input_data;
 	OutputData output_data;
 	aca::PID_Element pid_parameter_position = {5,0,0};
 	aca::PID_controller pid_position = aca::PID_controller (pid_parameter_position, frequency);
 	aca::PID_Element pid_parameter_angle {5,0,0};
 	aca::PID_controller pid_angle = aca::PID_controller (pid_parameter_angle, frequency);
-	void calc_initial_velocity();
+	void calc_initial_velocity();	//<! 砲弾の初速度を求める
 	void judge_locked_on();
 	void convert_to_SI(RobotSensorData& sensor_data, RobotMovementData& movement_data);
+	void calc_pos_of_target(RobotMovementData& mydata, RobotMovementData& enemydata);		//<! 本体の位置から的の位置を計算する
 public:
 	ArmoredTrain();
 	void update(InputData& input_data, OutputData& output_data);
--- 5m_unitbase/Core/Src/ArmoredTrain.cpp
+++ 5m_unitbase/Core/Src/ArmoredTrain.cpp
@@ -9,19 +9,42 @@
 
 namespace at {
 
+/**
+ * コンストラクタ
+ */
 ArmoredTrain::ArmoredTrain() {
 //	pid_position = aca::PID_controller (pid_parameter_position, frequency);
 }
 
+/**
+ * 砲弾の初速度を計算する関数
+ */
 void ArmoredTrain::calc_initial_velocity() {
-
+	float abs_bullet_velocity = robot_static_data.radius_of_roller * myself.roller_rotation;
+	float cos_depression = std::cos(robot_static_data.angle_of_depression);
+	float sin_depression = std::sin(robot_static_data.angle_of_depression);
+	float cos_turret = std::cos(myself.angle_of_turret + M_PI_2);
+	float sin_turret = std::sin(myself.angle_of_turret + M_PI_2);
+	bullet_velocity.x = abs_bullet_velocity * cos_depression * cos_turret
+						+ myself.velocity
+						- robot_static_data.turret_length * myself.roller_rotation * sin_turret;
+	bullet_velocity.y = abs_bullet_velocity * cos_depression * sin_turret
+						+ robot_static_data.turret_length * myself.roller_rotation * cos_turret;
+	bullet_velocity.z = abs_bullet_velocity * sin_depression;
 }
 
+/**
+ * ロックオンされているかを判定する
+ */
 void ArmoredTrain:: judge_locked_on() {
 
 }
 
-
+/**
+ * センサーの入力を、SI単位系に変換する関数
+ * @param sensor_data
+ * @param movement_data
+ */
 void ArmoredTrain:: convert_to_SI(RobotSensorData& sensor_data, RobotMovementData& movement_data) {
 	static RobotSensorData prev_sensor_data;
 	movement_data.angle_of_turret			 = sensor_data.pot_angle_of_turret * 0.01;
@@ -31,6 +54,24 @@ void ArmoredTrain:: convert_to_SI(RobotSensorData& sensor_data, RobotMovementDat
 	movement_data.roller_rotation			 = (sensor_data.enc_roller_rotation	 - prev_sensor_data.enc_roller_rotation	) * 0.001 * frequency;
 }
 
+/**
+ * 本体位置から的位置を計算する
+ */
+void ArmoredTrain::calc_pos_of_target(RobotMovementData& mydata, RobotMovementData& enemydata){
+	for (uint8_t i = 0; i < 3; i++) {
+		// 砲塔についていないターゲット
+		float x = mydata.position + enemydata.position - field_data.rail_length + robot_static_data.target[i].x;
+		float y = field_data.opposing_distance - robot_static_data.target[i].y;
+		target[i].l = std::sqrt(x*x + y*y);
+		target[i].angle = std::atan2(x, y);
+	}
+}
+
+/**
+ * update関数
+ * @param input_data
+ * @param output_data
+ */
 void ArmoredTrain::update(InputData& input_data, OutputData& output_data) {
 	this->input_data = input_data;
 	convert_to_SI(input_data.myself, myself);
