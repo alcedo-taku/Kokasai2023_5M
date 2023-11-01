5fc1b5d08a060388e22ce107e2325c1231328c90
alcedo-taku <taku.tatewaki@gmail.com>
2023-11-02 06:08:06 +0900

    add(unit): 横移動の速度制限を追加

--- 5m_unitbase/Core/Src/ArmoredTrain.cpp
+++ 5m_unitbase/Core/Src/ArmoredTrain.cpp
@@ -246,12 +246,12 @@ void ArmoredTrain::calc_output(RobotMovementData& now, RobotMovementData& target
 	}
 	// 横移動
 	output_data.compare[2] = input_data.ctrl.right_handle * 0.8;
-//	if (output_data.compare[2] < 0) {
-//		output_data.compare[2] = suppress_value(output_data.compare[2], std::sqrt(now.position));
-//	}else {
-//		min_distance = FieldData::rail_length - now.position;
-//	}
-//	output_data.compare[2] = output_data.compare[2] *
+	output_data.compare[2] = suppress_value<int16_t>(output_data.compare[2], 2000);
+	if (output_data.compare[2] < 0) {
+		output_data.compare[2] = suppress_value<int16_t>(output_data.compare[2], now.position*2000);
+	}else {
+		output_data.compare[2] = suppress_value<int16_t>(output_data.compare[2], (FieldData::rail_length - now.position)*2000);
+	}
 	if (now.position <= 0 || FieldData::rail_length <= now.position) {
 		output_data.compare[2] = 0;
 	}
